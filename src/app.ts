d23850579cecf5892abc9681b62f270cb3e25889
João Batista <joao.ferreira@blueservice.com.br>
2025-11-27 18:15:53 -0300

    feat: created default project strucutre, TS and Docker configuration

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import helmet from 'helmet';
+import routes from "./routes";
+
+const app = express();
+app.use(helmet());
+app.use(express.json());
+app.use("/api", routes);
+
+export default app;
